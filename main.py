a0565c771bf12cde6b891fd4102e0369cb8e820e
AndiMue <andreasgmueller@t-online.de>
2023-12-11 19:44:33 +0100

    update 11.12.2023 4 Funktionen in ideal 50 gefunden, nächstes: test Daten mit 4 idealen kt verlgeichen und updaten in DB

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Lib.Data_import import DataToImport
 import sqlalchemy as sql
 from Lib.DataWriteToDB import DataToDatabase
 import os
+from Lib.Search_Function import lookupthings
 
 
 def extract_tablename(path):
@@ -10,35 +11,67 @@ def extract_tablename(path):
     return result
 
 
-DataBaseName = "T2.db"
+DataBaseName = "T3.db"
 
 engine = sql.create_engine("sqlite+pysqlite:///" + DataBaseName, echo=True)
 connection = engine.connect()
 
-DI = DataToImport()
-DW = DataToDatabase()
-print("TEST.csv")
+train_data = DataToImport()
+ideal_data = DataToDatabase()
+
+''' ideal.csv importieren und in DB bringen'''
+
+print("IDEAL.csv")
+ideal_data.importieren("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\ideal.csv")
+Tablename = extract_tablename("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\ideal.csv")
+ideal_data.create_table(Tablename, ideal_data, connection)
+ideal_data.data_to_table(Tablename, ideal_data, connection)
+# del ideal_data
+
+''' Trainingsdaten importieren und in DB bringen'''
+train_data.importieren("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\train.csv")
+Tablename = extract_tablename("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\train.csv")
+train_data_to_DB = DataToDatabase()
+train_data_to_DB.create_table(Tablename, train_data, connection)
+train_data_to_DB.data_to_table(Tablename, train_data, connection)
+
+
+''' Trainingsdaten in ideal.csv suchen '''
+
+ideal_4_data = lookupthings()
+ideal_4_data.lookup_train_in_ideal(train_data, ideal_data)
+ideal_4_data_to_DB = DataToDatabase()
+ideal_4_data_to_DB.create_table_id4('four_of_ideal', ideal_4_data, connection)  # eigtl nicht nötig
+ideal_4_data_to_DB.data_to_table_id4('four_of_ideal', ideal_4_data, connection)     # eigtl nicht nötig
+
+
+''' Testdaten importieren um mit idealen 4 zu vergleichen'''
+test_data = DataToImport()
+test_data_to_DB = DataToDatabase()
+test_data_compare = lookupthings()
 ''' import und Weiterverarbeitung mit Kind-Klasse '''
-DW.importieren("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\test.csv")
+test_data_to_DB.importieren("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\test.csv")
 Tablename = extract_tablename("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\test.csv")
-DW.create_table(Tablename, DW, connection)
-DW.data_to_table(Tablename, DW, connection)
-del DI
+test_data_compare.lookup_train_in_ideal(test_data, ideal_4_data)
+
+# test_data_to_DB.create_table(Tablename, test_data_to_DB, connection)
+# test_data_to_DB.data_to_table(Tablename, test_data_to_DB, connection)
+del test_data
 
 ''' import und Weiterverarbeitung mit Eltern-Klasse '''
 DI = DataToImport()
 DI.importieren("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\train.csv")
 Tablename = extract_tablename("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\train.csv")
-DW.create_table(Tablename, DI, connection)
-DW.data_to_table(Tablename, DI, connection)
+# DW.create_table(Tablename, DI, connection)
+# DW.data_to_table(Tablename, DI, connection)
 del DI
 
 DI = DataToImport()
 print("IDEAL.csv")
 DI.importieren("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\ideal.csv")
 Tablename = extract_tablename("C:\\_Data\\IU_Python-Kurs\\Git_Repos\\Beispiel-Datensaetze\\ideal.csv")
-DW.create_table(Tablename, DI, connection)
-DW.data_to_table(Tablename, DI, connection)
+# DW.create_table(Tablename, DI, connection)
+# DW.data_to_table(Tablename, DI, connection)
 del DI
 
 print("DONE")
--- venv/Lib/DataWriteToDB.py
+++ venv/Lib/DataWriteToDB.py
@@ -30,7 +30,26 @@ class DataToDatabase(DataToImport):
             for row in result:
                 rowsintable += 1  # i > 0 → Tabelle ist schon befüllt
         self.rows = rowsintable
-        
+
+    def create_table_id4(self, tablename, di, connection):
+        ####### text fuer Create bauen (x float, y0 float, y1 float, ... )
+        text_create = "(" + di.x[0] + " float, "
+        for i in range(1, di.Anzahl_Spalten-2):
+            text_create += di.y[1][i][0] + " float, "
+        text_create += di.y[1][3][0] + " string)"
+
+        rowsintable = 0
+
+        try:
+            connection.execute(sql.text("CREATE TABLE " + tablename + " " + text_create))
+        except sqlOpErr:
+            #    pass
+            print("Tabelle existiert schon")
+            result = connection.execute(sql.text("SELECT * FROM " + tablename))
+            for row in result:
+                rowsintable += 1  # i > 0 → Tabelle ist schon befüllt
+        self.rows = rowsintable
+        print("DONE Data_to_table id4")
     def data_to_table(self, tablename, di, connection):
         rowsintable = self.rows
 
@@ -53,6 +72,35 @@ class DataToDatabase(DataToImport):
                 dict2 = {di.x[0]: di.x[j]}
                 for i in range(1, di.Anzahl_Spalten):  # i steht für die Spalten
                     dict2.update({di.y[i][0]: di.y[i][j]})  # dictionary bauen
+                connection.execute(sql.text("INSERT INTO " + tablename + " " + text_insert + " VALUES " + text_values), [dict2])
+            connection.commit()
+        else:
+            print("Tabelle bereits befüllt")
+        pass
+
+
+    def data_to_table_id4(self, tablename, di, connection):
+        rowsintable = self.rows
+
+        if rowsintable == 0:
+            ####### TEXT_INSERT bauen (x, y1, y2, ...)
+            text_insert = "(" + di.x[0] + ", "
+            for i in range(di.Anzahl_Spalten - 2):
+                text_insert += di.y[1][i + 1][0] + ", "
+            text_insert = text_insert[:-2]
+            text_insert += ")"
+
+            ####### TEXT VALUES bauen VARIABEL  (:x, :y1, :y2, ...)
+            text_values = "(:" + di.x[0] + ", :"
+            for i in range(di.Anzahl_Spalten - 2):
+                text_values += di.y[1][i + 1][0] + ", :"
+            text_values = text_values[:-3]
+            text_values += ")"
+
+            for j in range(1, len(di.x)):  # j steht für die Zeilen
+                dict2 = {di.x[0]: di.x[j]}
+                for i in range(1, di.Anzahl_Spalten-1):  # i steht für die Spalten
+                    dict2.update({di.y[1][i][0]: di.y[1][i][j]})  # dictionary bauen
                 connection.execute(sql.text("INSERT INTO " + tablename + " " + text_insert + " VALUES " + text_values), [dict2])
             connection.commit()
         else:
